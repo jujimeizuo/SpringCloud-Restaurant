2179ad6dd9f946f9a0b29c1e509c446fcc89f51a
jujimeizuo <3058852685@qq.com>
2022-01-01 22:07:35 +0800

    update

---
+++ dish/src/main/java/com/zjgsu/DishApplication.java
@@ -0,0 +1,13 @@
+package com.zjgsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DishApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DishApplication.class, args);
+    }
+}
---
+++ erueka/src/main/java/com/zjgsu/EruekaApplication.java
@@ -0,0 +1,13 @@
+package com.zjgsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EruekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EruekaApplication.class, args);
+    }
+}
