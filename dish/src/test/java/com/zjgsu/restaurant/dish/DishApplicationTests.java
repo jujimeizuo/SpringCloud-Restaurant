7e79f26c2a24aa46de65ffb78aa5ea4db1359fa2
jujimeizuo <3058852685@qq.com>
2022-01-01 20:11:23 +0800

    add login and dish

---
+++ dish/src/test/java/com/zjgsu/restaurant/dish/DishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjgsu.restaurant.dish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
