32dc89f7dfe871642a57952d4e54dd714a436e46
jujimeizuo <3058852685@qq.com>
2022-01-01 19:20:25 +0800

    add more port

---
+++ comsumer/src/main/java/com/zjgsu/restaurant/comsumer/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.zjgsu.restaurant.comsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/hello")
+    public String hello() {
+        String forObject = restTemplate.getForObject("http://client/student/detail", String.class);
+        return forObject;
+    }
+}
---
+++ comsumer/src/test/java/com/zjgsu/restaurant/comsumer/ComsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjgsu.restaurant.comsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
