cfdf25b4704cdc8a6c9e6363dfec9a9bd6068ff4
jujimeizuo <3058852685@qq.com>
2022-01-01 19:42:39 +0800

    update comsumer

--- comsumer/src/main/java/com/zjgsu/restaurant/comsumer/controller/HelloController.java
+++ comsumer/src/main/java/com/zjgsu/restaurant/comsumer/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+
 public class HelloController {
     @Autowired
     private RestTemplate restTemplate;
