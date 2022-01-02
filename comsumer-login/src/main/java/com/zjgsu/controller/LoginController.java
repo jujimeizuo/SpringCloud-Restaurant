9e87ba2309874ae7ebbe1550eab8c86d8c0af4db
jujimeizuo <3058852685@qq.com>
2022-01-02 16:28:49 +0800

    update log

--- comsumer-login/src/main/java/com/zjgsu/controller/LoginController.java
+++ comsumer-login/src/main/java/com/zjgsu/controller/LoginController.java
@@ -1,9 +1,15 @@
 package com.zjgsu.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.zjgsu.entity.UserAddRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,8 +19,26 @@ import java.util.Map;
 @DefaultProperties(defaultFallback = "defaultFallback")
 public class LoginController {
 
+    @Autowired
+    private RestTemplate restTemplate;
 
+    @PostMapping("/reg")
+    @HystrixCommand
+    public Object reg(@RequestBody UserAddRequest userAddRequest) {
+        Object o = restTemplate.postForObject("http://login/reg", userAddRequest, Object.class);
+        assert o != null;
+        log.info(o.toString());
+        return o;
+    }
 
+    @PostMapping("log")
+    @HystrixCommand
+    public Object log(@RequestBody UserAddRequest userAddRequest) {
+        Object o = restTemplate.postForObject("http://login/log", userAddRequest, Object.class);
+        assert o != null;
+        log.info(o.toString());
+        return o;
+    }
 
     /**
      *  服务响应超时的回调
